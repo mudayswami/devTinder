c9340405259861da0d1f2d14ca263674f4390308
Anubhav <anubhav.kumar@ixambee.com>
2025-01-16 23:49:15 +0530

    routing

--- src/app.js
+++ src/app.js
@@ -2,6 +2,24 @@ const express = require("express");
 
 const app = express();
 
+
+app.get('/us+er', (req, res) =>{
+    res.send ({firstName:"Anubhav"})
+});
+
+app.post("/user", (req,res) => {
+    res.send("Data saved successfully");
+});
+
+
+app.patch("/user", (req,res) => {
+    res.send("patch success");
+})
+
+app.delete("/user", (req,res) => {
+    res.send("Update");
+});
+
 app.use("d",(req,res) =>  {
     res.send("Hello from the server");
 });
